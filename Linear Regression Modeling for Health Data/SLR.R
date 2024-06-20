d306d1c8e5b6dbb0701de12d044b7f929faa8c3e
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2024-06-20 12:01:00 +0800

    Add files via upload

--- Linear Regression Modeling for Health Data/SLR.R
+++ Linear Regression Modeling for Health Data/SLR.R
@@ -1,3 +1,28 @@
 library(healthds)
 library(tidyverse)
+
+# Quiz 1
+set.seed(1234)
+sims = simulate_t(rep = 1000, mean1 = 3.0, mean2 = 3.0, n1 = 25, n2 = 25, sd = 0.5)
+hist(sims$t)
+
+library(healthds)
+set.seed(1234)
+sims = simulate_t(rep = 1000, mean1 = 4.5, mean2 = 3.0, 
+                  n1 = 25, n2 = 25, sd = 0.5)
+hist(sims$t)
+mean(sims$t)
+
+sims = simulate_t(rep = 1000, mean1 = 4.5, mean2 = 3.0, 
+                  n1 = 25, n2 = 25, sd = 0.5)
+curve(dt(x, df = 48), col="darkblue", lwd=2, yaxt="n", 
+      from = -2, to = 16)
+hist(sims$t, add = TRUE, probability = TRUE)
+
+
+
+
+
+
+
 data("BPUrban300")
